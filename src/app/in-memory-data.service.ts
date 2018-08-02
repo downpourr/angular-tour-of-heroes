a3006179a91104ae29d18ab36030817bb02ae3d9
root <root@centos7.fenestros.loc>
2018-08-03 00:35:02 +0200

    ajout d'un hero

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -11,7 +11,7 @@ export class InMemoryDataService {
       { id: 18, name: 'Dr IQ' },
       { id: 19, name: 'Magma' },
       { id: 20, name: 'Tornado' },
-			{ id: 21, name: 'Batman' },
+			{ id: 21, name: 'batman' },
     ];
     return { heroes };
   }
