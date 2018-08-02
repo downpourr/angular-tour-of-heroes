45bd6710ab56cfe8dcf936a99d4f56faa8af39ed
root <root@centos7.fenestros.loc>
2018-08-03 00:47:30 +0200

    traduction

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -11,7 +11,7 @@ export class InMemoryDataService {
       { id: 18, name: 'Dr IQ' },
       { id: 19, name: 'Magma' },
       { id: 20, name: 'Tornado' },
-			{ id: 21, name: 'batman' },
+			{ id: 21, name: 'batmanmm' },
     ];
     return { heroes };
   }
